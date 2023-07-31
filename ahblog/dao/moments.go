011853e12ba8861050e410894f8a30a042a08ae3
尸府主任 <gfiszhang@gmail.com>
2023-07-31 16:41:58 +0800

    将 ahblog 后台剥离为独立仓库

--- ahblog/dao/moments.go
+++
@@ -1,7 +0,0 @@
-package dao
-
-// Moments 内部引用
-type Moments struct {
-	From string
-	To   string
-}
--- ahblog/server/default.go
+++
@@ -1,5 +0,0 @@
-package server
-
-import "github.com/gin-gonic/gin"
-
-func notFound(ctx *gin.Context) {}
