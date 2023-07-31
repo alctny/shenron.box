d64338e85ae38e4a81352385d8d226452afb12e4
尸府主任 <gfiszhang@gmail.com>
2023-07-31 16:32:53 +0800

    不在rsfn 的文件名不再小写

--- rsfn/src/main.rs
+++ rsfn/src/main.rs
@@ -27,7 +27,7 @@ fn rsfn(p: &str, pam: &str, to: &str) {
         match file.file_type().unwrap().is_dir() {
             true => {
                 let file_name = &file.file_name().to_string_lossy().to_string();
-                let new_file_name = term(&file_name.replace(pam, to)).to_lowercase();
+                let new_file_name = term(&file_name.replace(pam, to));
                 let old_path = Path::new(p).join(file_name).to_string_lossy().to_string();
                 let new_path = Path::new(p)
                     .join(new_file_name)
@@ -38,7 +38,7 @@ fn rsfn(p: &str, pam: &str, to: &str) {
             }
             false => {
                 let file_name = &file.file_name().to_string_lossy().to_string();
-                let new_file_name = term(&file_name.replace(pam, to)).to_lowercase();
+                let new_file_name = term(&file_name.replace(pam, to));
                 let old_path = Path::new(p).join(file_name).to_string_lossy().to_string();
                 let new_path = Path::new(p)
                     .join(new_file_name)
